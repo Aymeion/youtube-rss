0cab5e4fc96deeff07cf2efe4be4796191e1f0ee
Aymeion <82944194+Aymeion@users.noreply.github.com>
2026-01-09 10:37:52 +0100

    Added playlists

--- config.py
+++ config.py
@@ -4,8 +4,11 @@ PLAYLISTS = {
     "fully_ramblomatic": "PLUBKwq0XD0ueR3CXGUhGpsD1puLcYJPUp",
     "semi_ramblomatic": "PLUBKwq0XD0uejtHnqX-LK14lEMAwWQk7s",
     "yahtzee_tries": "PLUBKwq0XD0uekGMJmHwioeoldreCjQTBc",
-    "canard_pc_emission": "PLDN-m4HWH8MCwmKac1e-0OkD6vk-hTFCX",
     "jon_stewart": "PLeskMkEaHJYfTMaKwmBWGq4NogX9FLF2J",
+    "canard_pc_emission": "PLDN-m4HWH8MCwmKac1e-0OkD6vk-hTFCX",
+    "ctrl_technique": "PLDN-m4HWH8MCCH2LAyYjmtv8Sgmiftylu",
+    "tribunal_des_bureaux": "PLDN-m4HWH8MBKJLYIK-80qJBBkVJbPo9p",
+    "live_laugh_love": "PLDN-m4HWH8MDbO5GHjeJcB38TGI6fm84u",
 }
 
 NB_DAYS = 60
